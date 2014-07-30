265ea8cd8691ba4c5ae1e70951a9b3deb446a98f
djewiss <djewiss@paradise.net.nz>
2014-07-30 20:25:14 +1200

    Update unicast-beacon.c

--- unicast-beacon.c
+++ unicast-beacon.c
@@ -1,54 +1,9 @@
-/*
- * Copyright (c) 2010, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
 /**
  * \file
- *         Demonstrates how to use broadcast and unicast
+ *         unicast location beacon
  * \author
- *         Adam Dunkels <adam@sics.se>
+ *         Darryl Jewiss
  *
- *         This example shows how to send broadcast and unicast, as
- *         well as how to use the Contiki memory block manager (MEMB)
- *         and the Contiki list library (LIST) to keep track of
- *         neighbors. The program consists of two processes, one that
- *         periodically sends broadcast messages and one that
- *         periodically sends unicast messages to random neighbors. A
- *         list of neighbors is maintained. The list is populated from
- *         the reception of broadcast messages from neighbors. The
- *         neighbor list keeps a simple set of quality metrics for
- *         each neighbor: a moving average of sequence number gaps,
- *         which indicates the number of broadcast packets that have
- *         been lost; a the last RSSI received; and the last LQI
- *         received.
  */
 
 

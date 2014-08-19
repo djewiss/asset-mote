e90a6bed2199bad627c204e6e4fd6a684228fc41
djewiss <djewiss@paradise.net.nz>
2014-08-19 22:09:05 +1200

    Update location-beacon.c

--- location-beacon.c
+++ location-beacon.c
@@ -14,13 +14,6 @@ static void callback(struct websocket *s, websocket_result r,
 #define RECONNECT_INTERVAL 20 * CLOCK_SECOND
 static struct ctimer reconnect_timer;
 
-#define PORT 12345
-
-static struct udp_socket u;
-static uip_ipaddr_t addr;
-
-#define SEND_INTERVAL		(30 * CLOCK_SECOND)
-static struct etimer periodic_timer, send_timer;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(blink_process, "Blink process");
@@ -49,39 +42,14 @@ callback(struct websocket *s, websocket_result r,
     ctimer_set(&reconnect_timer, RECONNECT_INTERVAL, reconnect_callback, s);
   } else if(r == WEBSOCKET_CONNECTED) {
     websocket_send_str(s, "Connected");
-    websocket_send_str(s, "{\"TagNumberFrom\":\"darryl_test\",\"TagNumberTarget\":\"\",\"Status\":\"Started\",\"Location\":\"\",\"EquipmentSerialNumber\":\"\"}");
+    websocket_send_str(s, "{\"TagNumberFrom\":\"\",\"TagNumberTarget\":\"\",\"Status\":\"Started\",\"Location\":\"Gracefield\",\"EquipmentSerialNumber\":\"\"}");
   } else if(r == WEBSOCKET_DATA) {
     printf("websocket: Received data '%.*s' (len %d)\n", datalen,
            data, datalen);
   }
 }
 /*---------------------------------------------------------------------------*/
-static void
-route_callback(int event, uip_ipaddr_t *route, uip_ipaddr_t *ipaddr,
-               int numroutes)
-{
-  if(event == UIP_DS6_NOTIFICATION_DEFRT_ADD) {
-    leds_off(LEDS_ALL);
-    printf("Got a RPL route\n");
-  }
-}
 
-static void
-receiver(struct udp_socket *c,
-         void *ptr,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
-{
-  printf("Data received on port %d from port %d with length %d, '%s'\n",
-         receiver_port, sender_port, datalen, data);
-  printf("{\" 00 12 4B 00 04 0E F1 F2\":\"Gracefield\",\"Tags\":[\"00 12 4B 00 04 0E F3 88\" ]}");
-  websocket_send_str(&s,"{\" 00 12 4B 00 04 0E F1 F2\":\"Gracefield\",\"Tags\":[\"00 12 4B 00 04 0E F3 88\" ]}");
-  
-}
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(websocket_example_process, ev, data)
 {
@@ -98,7 +66,7 @@ PROCESS_THREAD(websocket_example_process, ev, data)
 
   PROCESS_END();
 }
-
+/*-----------------------------------------------------------------------------*/
 PROCESS_THREAD(blink_process, ev, data)
 {
     /* The event timer must be declared as static. */
@@ -122,47 +90,5 @@ PROCESS_THREAD(blink_process, ev, data)
 
     /* Processes must end with PROCESS_END(). */
     PROCESS_END();
-}
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(multicast_example_process, ev, data)
-{
-  PROCESS_BEGIN();
-
-  /* Create a linkl-local multicast addresses. */
-  uip_ip6addr(&addr, 0xff02, 0, 0, 0, 0, 0, 0x1337, 0x0001);
-
-  /* Join local group. */
-  if(uip_ds6_maddr_add(&addr) == NULL) {
-    printf("Error: could not join local multicast group.\n");
-  }
-
-  /* Register UDP socket callback */
-  udp_socket_register(&u, NULL, receiver);
-
-  /* Bind UDP socket to local port */
-  udp_socket_bind(&u, PORT);
-
-  /* Connect UDP socket to remote port */
-  udp_socket_connect(&u, NULL, PORT);
-
-  while(1) {
-
-    /* Set up two timers, one for keeping track of the send interval,
-       which is periodic, and one for setting up a randomized send time
-       within that interval. */
-    etimer_set(&periodic_timer, SEND_INTERVAL);
-    etimer_set(&send_timer, (random_rand() % SEND_INTERVAL));
-
-    PROCESS_WAIT_UNTIL(etimer_expired(&send_timer));
-
-    printf("Sending multicast\n");
-    udp_socket_sendto(&u,
-                      " 00 12 4B 00 04 0E F1 F2", 24,
-                      &addr, PORT);
-
-    PROCESS_WAIT_UNTIL(etimer_expired(&periodic_timer));
-  }
-
-  PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
